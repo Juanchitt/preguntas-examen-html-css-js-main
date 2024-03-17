c72fed94bd11a6922babea9fcf4e526cd4dc26e2
Juanchitt <140765072+Juanchitt@users.noreply.github.com>
2024-03-17 17:53:00 -0500

    firts commit

---
+++ preguntas.js
@@ -0,0 +1,167 @@
+let questions = [
+  {
+  numb: 1,
+  question: "In what is the Colombia Very well program based?",
+  answer: "It is based on the improvement of English at the national level of basic upper middle and working",
+  options: [
+    "It is based on the improvement of English at the national level of basic upper middle and working",
+    "It is based on teachers having a good personal level of language",
+    "Students can only write in English    ",
+    "All of the above"
+  ]
+},
+  {
+  numb: 2,
+  question: "According to the first initiative, depending on the results obtained from the diagnostic test, the teacher can access courses:",
+  answer: "All of the above",
+  options: [
+    "of 200 to 500 hours of English lessons.",
+    "To immersion programs in English",
+    "To training programs in methodology to transform their pedagogical practice.",
+    "All of the above"
+  ]
+},
+  {
+  numb: 3,
+  question: "How many key initiatives are proposed to ensure a solid foundation for all children in Colombia?",
+  answer: "13",
+  options: [
+    "12",
+    "13",
+    "5",
+    "8"
+  ]
+},
+  {
+  numb: 4,
+  question: "What are the six basic components of the programmes for quality education?",
+  answer: "all of the above",
+  options: [
+    "Teacher support and training - Complete pedagogical model",
+    "Support infrastructure and technology - Performance culture",
+    "Incorporating the English language outside school-Integration and synergy",
+    "all of the above"
+  ]
+},
+  {
+  numb: 5,
+  question: "What proposes the fifth accelerated intervention?",
+  answer: "Summer camps, extended days, remedial support, extracurricular activities in English ",
+  options: [
+    "Strengthen digital teaching skills -  Enable remote teacher strategy - Enable co-teaching strategy with foreign volunteers and/or teachers",
+    "Hourly intensity of English as a subject for primary school- Extend hourly intensity in secondary and middle school - Support Secretariats considering making official bilingual schools to identify minimum conditions for success",
+    "Performance culture- Conduct oral proficiency assessments to students-  Familiarisation and motivation to English (e.g. Bilingual and Official Private Integration)",
+    "Summer camps, extended days, remedial support, extracurricular activities in English "
+  ]
+},
+{
+  numb: 6,
+  question: "What does the sixth initiative suggest?",
+  answer: "Considers integration and synergy between sectors",
+  options: [
+    "integration",
+    "Synergy between sectors",
+    "Develop alliances with territorial entities",
+    "Considers integration and synergy between sectors"
+  ]
+},
+{
+  numb: 7,
+  question: "What do the accelerated initiatives are for?",
+  answer: "achieving faster results",
+  options: [
+    "longer results and learning",
+    "implementing educational resources faster",
+    "achieving faster results",
+    "none of the above"
+  ]
+},
+{
+  numb: 8,
+  question: "What are the alliances with different means of communication with a high potential impact to strengthen the exposure to English?",
+  answer: "all of the above",
+  options: [
+    "National and Regional",
+    "audiovisual",
+    "radial and written",
+    "all of the above"
+  ]
+},
+{
+  numb: 9,
+  question: "How will the aids be implemented in the acceleration projects?",
+  answer: "with a thematic and regional coordination team",
+  options: [
+    "with resources from the government",
+    "with a thematic and regional coordination team",
+    "finding a teacher-only implementation",
+    "none of the above"
+  ]
+},
+{
+  numb: 10,
+  question: "What are some of the alternative strategies for the gap closure?",
+  answer: "All of the above",
+  options: [
+    "Availability of extra hours for English teachers",
+    "Presence of quality English Bachelor programs - Practitioners in the classroom",
+    "Good infrastructure and connectivity - Remote teachers",
+    "All of the above"
+  ]
+},
+{
+  numb: 11,
+  question: "In the creation of a virtuous circle, which motivates and supports the formation of English, the characterization of English requirements requires:",
+  answer: "incorporate the requirements of English into the development of the Common Qualifications Framework",
+  options: [
+    "To encourage and create the need for training in English ",
+    "incorporate the requirements of English into the development of the Common Qualifications Framework",
+    "facilitate access to resources",
+    "none of the above"
+  ]
+},
+{
+  numb: 12,
+  question: "What is the objective of the Complete Pedagogical Model in an innovative education?",
+  answer: "Extend hourly intensity in secondary and middle",
+  options: [
+    "Extend hourly intensity in secondary and middle",
+    "Extracurricular activities in English",
+    "Enable remote teacher strategy",
+    "English laboratories for immersion with technology support "
+  ]
+},
+{
+  numb: 13,
+  question: "What are some of the awards and incentives for those who commit to dedicate 200 hours a year to training in English",
+  answer: "all of the above",
+  options: [
+    "Possibility to apply to outdoor immersion (e.g.: USA)",
+    "Possibility to apply for education abroad",
+    "Possibility of transition to Tutor ",
+    "all of the above"
+  ]
+},
+{
+  numb: 14,
+  question: "Capacity building strategies.  (Which of the following in correct)",
+  answer: "D is incorrect",
+  options: [
+    "(A) Talleres en grupo de formación en metodología  (B) Soporte metodológico vía web  (C) Comunidades de docentes (D)   Talleres de refuerzo estudiantil  (E) Tutoring in the classroom ",
+    "A and B are correct",
+    "C and D are correct",
+    "D is incorrect"
+  ]
+},
+{
+  numb: 15,
+  question: "English, please! is a high quality series developed for grades ________ through which teachers can transform the teaching and learning processes of English as a foreign language and ensure better results.",
+  answer: "A and C are correct",
+  options: [
+    "9 and 10",
+    "10 and 11",
+    "11",
+    "A and C are correct"
+  ]
+},
+];
